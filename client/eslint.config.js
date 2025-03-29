30d53fc7cfa652ea82089cca830a92abfe1f6303
kenwoo9y <kenwoo9y@gmail.com>
2025-03-30 00:01:00 +0900

    setup eslint and prettier

---
+++ client/eslint.config.js
@@ -0,0 +1,28 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import js from '@eslint/js';
+
+const compat = new FlatCompat({
+    recommendedConfig: js.configs.recommended
+});
+
+export default [
+    {
+        ignores: ['**/dist/**']
+    },
+    ...compat.config({
+        extends: [
+            'eslint:recommended',
+        ]
+    }),
+    eslintConfigPrettier,
+    {
+        plugins: {
+            vue: eslintPluginVue
+        },
+        rules: {
+            'vue/multi-word-component-names': 'off',
+        },
+    },
+];
