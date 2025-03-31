4162157b819a56aad796e6a52ff07b91b4fa2b9d
kenwoo9y <kenwoo9y@gmail.com>
2025-03-31 15:22:46 +0900

    setup vitest

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', 'nuxt-mdi'],
+  modules: ['vuetify-nuxt-module', 'nuxt-mdi', '@nuxt/test-utils/module'],
   mdi: {
     componentName: 'MdiIcon',
     defaultSize: '1.5em',
---
+++ client/vitest.config.ts
@@ -0,0 +1,31 @@
+// vitest.config.ts
+import { fileURLToPath } from 'node:url';
+import { defineVitestConfig } from '@nuxt/test-utils/config';
+
+export default defineVitestConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'nuxt',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+      exclude: [
+        '**/virtual:nuxt:**',
+        '**/.nuxt/**',
+        '**/node_modules/**',
+        '**/*.config.*',
+        '**/*.test.*',
+      ],
+    },
+  },
+});
