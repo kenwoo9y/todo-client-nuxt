b468b2a977e1f2c5706c00f40b703fff813d2563
kenwoo9y <kenwoo9y@gmail.com>
2025-03-31 14:40:34 +0900

    use NuxtLayout

--- client/app.vue
+++ client/app.vue
@@ -1,14 +1,7 @@
 <template>
   <v-app>
-    <Header />
-    <v-main>
+    <NuxtLayout>
       <NuxtPage />
-    </v-main>
-    <Footer />
+    </NuxtLayout>
   </v-app>
 </template>
-
-<script setup>
-import Header from '@/layouts/Header.vue'
-import Footer from '@/layouts/Footer.vue'
-</script>
---
+++ client/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <v-main>
+      <slot />
+    </v-main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/layouts/Header.vue'
+import Footer from '@/layouts/Footer.vue'
+</script>
