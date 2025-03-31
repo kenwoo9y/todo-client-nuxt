cc05d8b8804d3f5aa51a74d6fe6803f95c062e0f
kenwoo9y <kenwoo9y@gmail.com>
2025-03-31 13:42:51 +0900

    installed nuxt-mdi

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    'vuetify-nuxt-module'
-  ]
+    'vuetify-nuxt-module',
+    'nuxt-mdi'
+  ],
+  mdi: {
+    componentName: 'MdiIcon',
+    defaultSize: '1.5em'
+  }
 });
