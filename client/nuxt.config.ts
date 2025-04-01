93b19a1f218b108f50bec13535eb4a2bc466f0f3
kenwoo9y <kenwoo9y@gmail.com>
2025-04-01 22:20:22 +0900

    setup pinia

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', 'nuxt-mdi', '@nuxt/test-utils/module'],
+  modules: [
+    'vuetify-nuxt-module',
+    'nuxt-mdi',
+    '@nuxt/test-utils/module',
+    '@pinia/nuxt',
+  ],
   mdi: {
     componentName: 'MdiIcon',
     defaultSize: '1.5em',
